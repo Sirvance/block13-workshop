f75b5c2be1039402aa20d88c5250227d5fb274b2
vance <vancedowdell2110@yahoo.com>
2023-07-22 14:51:45 -0700

    new

--- index.js
+++ index.js
@@ -57,24 +57,6 @@ function checkIfBothGreaterOrEqual(num1, num2) {
 
  
 
-// Function to test the truthiness/falsiness of a value and print the result
-        function testTruthyFalsy(value) {
-            if (value) {
-                console.log(true);
-            } else {
-                if (typeof value === "boolean") {
-                    console.log("The boolean value false is falsy");
-                } else if (value === null) {
-                    console.log("The null value is falsy");
-                } else if (value === undefined) {
-                    console.log("undefined is falsy");
-                } else if (typeof value === "number" && value === 0) {
-                    console.log("The number 0 is falsy (the only falsy number)");
-                } else if (typeof value === "string" && value === "") {
-                    console.log("The empty string is falsy (the only falsy string)");
-                }
-            }
-        
 
 
 
@@ -91,4 +73,4 @@ function checkIfAtLeastOneTruthy(param1A, param1B, param2A, param2B) {
     } else {
         console.log("false");
     }
-}}
+}
