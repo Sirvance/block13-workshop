55b90c7221860210f1f20ae3840d772826b5f844
Sirvance <126606183+Sirvance@users.noreply.github.com>
2023-07-21 20:56:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,25 @@ if (sum > 100) {
     print(sum + " is less than -1000");
 }
 // Output: "101 is greater than 100"
+
+
+ 
+
+// Function to test the truthiness/falsiness of a value and print the result
+        function testTruthyFalsy(value) {
+            if (value) {
+                console.log(true);
+            } else {
+                if (typeof value === "boolean") {
+                    console.log("The boolean value false is falsy");
+                } else if (value === null) {
+                    console.log("The null value is falsy");
+                } else if (value === undefined) {
+                    console.log("undefined is falsy");
+                } else if (typeof value === "number" && value === 0) {
+                    console.log("The number 0 is falsy (the only falsy number)");
+                } else if (typeof value === "string" && value === "") {
+                    console.log("The empty string is falsy (the only falsy string)");
+                }
+            }
+        }
