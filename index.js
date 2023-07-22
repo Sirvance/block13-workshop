d8c40c07573c76a870ae2326a1b8e65d39d0fa6d
vance <vancedowdell2110@yahoo.com>
2023-07-22 14:44:33 -0700

    new

--- index.js
+++ index.js
@@ -16,3 +16,57 @@ if (sum > 100) {
     print(sum + " is less than -1000");
 }
 // Output: "101 is greater than 100"
+
+
+
+
+
+ // make the variable:value
+ //read inputs for value
+ // If 'value' is truthy then:Print "true"
+ //Else if 'value' is false then:print expected print of value
+ function testTruthyFalsy(value) {
+    if (value) {
+        console.log(true);
+    } else {
+        if (typeof value === "boolean") {
+            console.log("The boolean value false is falsy");
+        } else if (value === null) {
+            console.log("The null value is falsy");
+        } else if (value === undefined) {
+            console.log("undefined is falsy");
+        } else if (typeof value === "number" && value === 0) {
+            console.log("The number 0 is falsy (the only falsy number)");
+        } else if (typeof value === "string" && value === "") {
+            console.log("The empty string is falsy (the only falsy string)");
+        }
+    }
+}
+
+//make variables num1 and num2
+//If num1 >= 5 and num2 >= 5 then: print "true"
+//else:print"false
+function checkIfBothGreaterOrEqual(num1, num2) {
+    if (num1 >= 5 && num2 >= 5) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
+
+
+
+
+
+
+//make variables (param1A, param1B, param2A, param2B)
+//read input for variables
+//If (param1A is truthy OR param1B is truthy) OR (param2A is truthy OR param2B is truthy) then:Print "true"
+//Else:Print "false"
+function checkIfAtLeastOneTruthy(param1A, param1B, param2A, param2B) {
+    if ((param1A || param1B) || (param2A || param2B)) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
